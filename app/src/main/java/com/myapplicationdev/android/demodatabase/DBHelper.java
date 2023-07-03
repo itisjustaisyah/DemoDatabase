ea928cff84bbf24db703ee7bbecb35950bef1999
itisjustaisyah <123053209+itisjustaisyah@users.noreply.github.com>
2023-07-03 15:56:49 +0800

    final comit

--- app/src/main/java/com/myapplicationdev/android/demodatabase/DBHelper.java
+++ app/src/main/java/com/myapplicationdev/android/demodatabase/DBHelper.java
@@ -33,7 +33,7 @@ public class DBHelper extends SQLiteOpenHelper {
         ArrayList<Task> tasks = new ArrayList<>();
         SQLiteDatabase db = this.getReadableDatabase();
         String[] columns = {COLUMN_ID, COLUMN_DESCRIPTION, COLUMN_DATE};
-        Cursor cursor = db.query(TABLE_TASK, columns, null, null, null, null, null, null);
+        Cursor cursor = db.query(TABLE_TASK, columns, null, null, null, null, COLUMN_DESCRIPTION + " asc", null);
 
         if (cursor.moveToFirst()) {
             Log.i("cursor", "moveToFirst() false");
@@ -91,19 +91,6 @@ public class DBHelper extends SQLiteOpenHelper {
         // Close the database connection
         Log.i("info", "Task inserted");
         db.close();                                         
-
-    }
-    public void clearTask (){
-        // Get an instance of the database for writing
-        SQLiteDatabase db = this.getWritableDatabase();
-        // We use ContentValues object to store the values for
-        //  the db operation
-        ContentValues values = new ContentValues();
-        db.execSQL("DELETE FROM " + TABLE_TASK);
-        Log.i("Clear", "Cleared Table");
-        db.close();
-        // Store the column name as key and the description as value
-
 
     }
 }
--- app/src/main/java/com/myapplicationdev/android/demodatabase/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demodatabase/MainActivity.java
@@ -1,18 +1,16 @@
 package com.myapplicationdev.android.demodatabase;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import java.sql.Array;
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     Button btnInsert, btnGetTasks;
@@ -27,22 +25,21 @@ public class MainActivity extends AppCompatActivity {
         btnGetTasks = findViewById(R.id.btnGetTasks);
         tvResults = findViewById(R.id.tvResults);
         tv = findViewById(R.id.tv);
-        Button btnClear = findViewById(R.id.btnClear);
         DBHelper db = new DBHelper(MainActivity.this);
-        ArrayList<Task> data = db.getTasks();
-        ArrayAdapter<?> aaTask = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
-        tv.setAdapter(aaTask);
+        EditText editTask = findViewById(R.id.editTask);
+        EditText editDate = findViewById(R.id.editDate);
 
-        btnClear.setOnClickListener(v -> db.clearTask());
-        btnInsert.setOnClickListener(v -> db.insertTask("Submit RJ", "25 Apr 2021"));
+        btnInsert.setOnClickListener(v -> db.insertTask(editTask.getText().toString(), editDate.getText().toString()));
         btnGetTasks.setOnClickListener(v -> {
-
+            ArrayList<Task> data = db.getTasks();
+            ArrayAdapter<?> aaTask = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+            tv.setAdapter(aaTask);
             db.close();
             String txt = "";
 //            String txtListItem = "";
             for (int i = 0; i < data.size(); i++) {
                 Log.d("Database Content", i + ". " + data.get(i));
-                txt += i + ". "+ data.get(i) + "\n";
+                txt += data.get(i) + "\n";
                 /*txtListItem += i + "\n"
                         + data.get(i).getDecription() + "\n"
                         + data.get(i).getDate() + "\n";*/
